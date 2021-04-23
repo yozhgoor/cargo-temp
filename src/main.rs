21a0c1ab86f562aecbe51752e19123752a9bee7e
Yozhgoor <yozhgoor@outlook.com>
2021-04-23 12:05:37 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use anyhow::{bail, Context, Result};
+use clap::Clap;
+use std::io::Write;
+use std::{fs, process};
+use tempfile::Builder;
+
+#[derive(Clap, Debug)]
+struct Cli {
+    dependencies: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let cache_dir = dirs::cache_dir()
+        .context("Could not get cache directory")?
+        .join(env!("CARGO_PKG_NAME"));
+
+    let _ = fs::create_dir_all(&cache_dir);
+
+    let tmp_dir = Builder::new().prefix("tmp-").tempdir_in(&cache_dir)?;
+
+    if !process::Command::new("cargo")
+        .current_dir(&tmp_dir)
+        .arg("init")
+        .status()
+        .context("Could not start cargo")?
+        .success()
+    {
+        bail!("Cargo command failed");
+    }
+
+    let mut toml = fs::OpenOptions::new()
+        .append(true)
+        .open(&tmp_dir.path().join("Cargo.toml"))?;
+    for s in cli.dependencies.iter() {
+        writeln!(toml, "{} = \"*\"", s)?;
+    }
+    drop(toml);
+
+    process::Command::new(get_shell())
+        .current_dir(&tmp_dir)
+        .status()
+        .context("Cannot start shell")?;
+
+    Ok(())
+}
+
+fn get_shell() -> String {
+    #[cfg(unix)]
+    {
+        std::env::var("SHELL").unwrap_or_else(|_| "/bin/sh".to_string())
+    }
+
+    #[cfg(not(unix))]
+    {
+        compile_error!(
+            "Only unix systems supported at the moment. \
+            Help would be appreciated =D"
+        )
+    }
+}
