2b2dd27ed86add1160974ddebe35f7065da35fff
Yohan Boogaert <yozhgoor@outlook.com>
2021-08-16 16:55:39 +0200

    Refactor the code for readability (#35)

---
+++ src/lib.rs
@@ -0,0 +1,181 @@
+use crate::config::Config;
+use anyhow::Result;
+use clap::Clap;
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+pub mod config;
+pub mod run;
+
+/// This tool allow you to create a new Rust temporary project in a temporary
+/// directory.
+///
+/// The dependencies can be provided in arguments (e.g.`cargo-temp anyhow
+/// tokio`). When the shell is exited, the temporary directory is deleted unless
+/// you removed the file `TO_DELETE`
+#[derive(Clap, Debug)]
+pub struct Cli {
+    /// Dependencies to add to `Cargo.toml`.
+    ///
+    /// The default version used is `*` but this can be replaced using `=`.
+    /// E.g. `cargo-temp anyhow=1.0.13`
+    #[clap(parse(from_str = parse_dependency))]
+    pub dependencies: Vec<Dependency>,
+
+    /// Create a library instead of a binary.
+    #[clap(long)]
+    pub lib: bool,
+
+    /// Name of the temporary crate.
+    #[clap(long = "name")]
+    pub project_name: Option<String>,
+
+    /// Create a temporary Git working tree based on the repository in the
+    /// current directory
+    #[clap(long = "worktree")]
+    pub worktree_branch: Option<Option<String>>,
+}
+
+impl Cli {
+    pub fn run(&self, config: &Config) -> Result<()> {
+        let tmp_dir = run::generate_tmp_project(
+            self.worktree_branch.clone(),
+            self.project_name.clone(),
+            self.lib,
+            config.temporary_project_dir.clone(),
+        )?;
+
+        run::add_dependencies_to_project(tmp_dir.path(), &self.dependencies)?;
+
+        let delete_file = run::generate_delete_file(tmp_dir.path())?;
+
+        run::start_shell(config, tmp_dir.path())?;
+
+        run::clean_up(delete_file, tmp_dir, self.worktree_branch.clone())?;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Dependency {
+    CrateIo {
+        name: String,
+        version: Option<String>,
+    },
+    Repository {
+        branch: Option<String>,
+        name: String,
+        rev: Option<String>,
+        url: String,
+    },
+}
+
+fn parse_dependency(s: &str) -> Dependency {
+    // This will change when `std::lazy` is released.
+    // See https://github.com/rust-lang/rust/issues/74465.
+    static RE: Lazy<Regex> = Lazy::new(|| {
+        Regex::new(r"^(?P<name>[^=]+)=(?P<version>((?P<url>\w+://([^:@]+(:[^@]+)?@)?[^#]+)(#branch=(?P<branch>.+)|#rev=(?P<rev>.+))?)|.+)$")
+            .unwrap()
+    });
+
+    if let Some(caps) = RE.captures(s) {
+        if let Some(url) = caps.name("url") {
+            Dependency::Repository {
+                name: caps.name("name").unwrap().as_str().to_string(),
+                url: url.as_str().to_string(),
+                branch: caps.name("branch").map(|x| x.as_str().to_string()),
+                rev: caps.name("rev").map(|x| x.as_str().to_string()),
+            }
+        } else {
+            Dependency::CrateIo {
+                name: caps.name("name").unwrap().as_str().to_string(),
+                version: caps.name("version").map(|x| x.as_str().to_string()),
+            }
+        }
+    } else {
+        Dependency::CrateIo {
+            name: s.to_string(),
+            version: None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod parse_dependency_tests {
+    use super::*;
+
+    #[test]
+    fn simple_dependency() {
+        assert_eq!(
+            parse_dependency("anyhow"),
+            Dependency::CrateIo {
+                name: "anyhow".to_string(),
+                version: None,
+            }
+        );
+    }
+
+    #[test]
+    fn dependency_with_version() {
+        assert_eq!(
+            parse_dependency("anyhow=1.0"),
+            Dependency::CrateIo {
+                name: "anyhow".to_string(),
+                version: Some("1.0".to_string()),
+            }
+        )
+    }
+
+    #[test]
+    fn repository_with_http() {
+        assert_eq!(
+            parse_dependency("anyhow=https://github.com/dtolnay/anyhow.git"),
+            Dependency::Repository {
+                name: "anyhow".to_string(),
+                url: "https://github.com/dtolnay/anyhow.git".to_string(),
+                branch: None,
+                rev: None,
+            }
+        )
+    }
+
+    #[test]
+    fn repository_with_ssh_repository() {
+        assert_eq!(
+            parse_dependency("anyhow=ssh://git@github.com/dtolnay/anyhow.git"),
+            Dependency::Repository {
+                name: "anyhow".to_string(),
+                url: "ssh://git@github.com/dtolnay/anyhow.git".to_string(),
+                branch: None,
+                rev: None,
+            }
+        )
+    }
+
+    #[test]
+    fn repository_with_branch() {
+        assert_eq!(
+            parse_dependency("anyhow=https://github.com/dtolnay/anyhow.git#branch=main"),
+            Dependency::Repository {
+                name: "anyhow".to_string(),
+                url: "https://github.com/dtolnay/anyhow.git".to_string(),
+                branch: Some("main".to_string()),
+                rev: None,
+            }
+        )
+    }
+
+    #[test]
+    fn with_rev() {
+        assert_eq!(
+            parse_dependency("anyhow=https://github.com/dtolnay/anyhow.git#rev=7e0f77a38"),
+            Dependency::Repository {
+                name: "anyhow".to_string(),
+                url: "https://github.com/dtolnay/anyhow.git".to_string(),
+                branch: None,
+                rev: Some("7e0f77a38".to_string()),
+            }
+        )
+    }
+}
---
+++ src/run.rs
@@ -0,0 +1,180 @@
+use crate::config::Config;
+use crate::Dependency;
+use anyhow::{ensure, Context, Result};
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::{env, fs, process};
+use tempfile::TempDir;
+
+pub fn generate_tmp_project(
+    worktree_branch: Option<Option<String>>,
+    project_name: Option<String>,
+    lib: bool,
+    temporary_project_dir: PathBuf,
+) -> Result<TempDir> {
+    let tmp_dir = {
+        let mut builder = tempfile::Builder::new();
+
+        if worktree_branch.is_some() {
+            builder.prefix("wk-");
+        } else {
+            builder.prefix("tmp-");
+        }
+
+        builder.tempdir_in(temporary_project_dir)?
+    };
+
+    let project_name = project_name.unwrap_or_else(|| {
+        tmp_dir
+            .path()
+            .file_name()
+            .unwrap()
+            .to_string_lossy()
+            .to_lowercase()
+    });
+
+    if let Some(maybe_branch) = worktree_branch.as_ref() {
+        let mut command = process::Command::new("git");
+        command.args(["worktree", "add"]);
+
+        match maybe_branch {
+            Some(branch) => command.arg(tmp_dir.path()).arg(branch),
+            None => command.arg("-d").arg(tmp_dir.path()),
+        };
+
+        ensure!(
+            command.status().context("Could not start git")?.success(),
+            "Cannot create working tree"
+        );
+    } else {
+        let mut command = process::Command::new("cargo");
+        command
+            .current_dir(&tmp_dir)
+            .args(["init", "--name", project_name.as_str()]);
+
+        if lib {
+            command.arg("--lib");
+        }
+
+        ensure!(
+            command.status().context("Could not start cargo")?.success(),
+            "Cargo command failed"
+        );
+    }
+
+    Ok(tmp_dir)
+}
+
+pub fn add_dependencies_to_project(tmp_dir: &Path, dependencies: &[Dependency]) -> Result<()> {
+    let mut toml = fs::OpenOptions::new()
+        .append(true)
+        .open(tmp_dir.join("Cargo.toml"))?;
+    for dependency in dependencies.iter() {
+        match dependency {
+            Dependency::CrateIo {
+                name: s,
+                version: v,
+            } => match &v {
+                Some(version) => writeln!(toml, "{} = \"{}\"", s, version)?,
+                None => writeln!(toml, "{} = \"*\"", s)?,
+            },
+            Dependency::Repository {
+                name,
+                url,
+                branch,
+                rev,
+            } => {
+                write!(toml, "{name} = {{ git = {url:?}", name = name, url = url)?;
+                if let Some(branch) = branch {
+                    write!(toml, ", branch = {:?}", branch)?;
+                }
+                if let Some(rev) = rev {
+                    write!(toml, ", rev = {:?}", rev)?;
+                }
+                writeln!(toml, " }}")?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn generate_delete_file(tmp_dir: &Path) -> Result<PathBuf> {
+    let delete_file = tmp_dir.join("TO_DELETE");
+    fs::write(
+        &delete_file,
+        "Delete this file if you want to preserve this project",
+    )?;
+
+    Ok(delete_file)
+}
+
+pub fn start_shell(config: &Config, tmp_dir: &Path) -> Result<()> {
+    let mut shell_process = match config.editor {
+        None => process::Command::new(get_shell()),
+        Some(ref editor) => {
+            let mut ide_process = process::Command::new(editor);
+            ide_process
+                .args(config.editor_args.iter().flatten())
+                .arg(tmp_dir);
+            ide_process
+        }
+    };
+
+    if env::var("CARGO_TARGET_DIR").is_err() {
+        if let Some(path) = &config.cargo_target_dir {
+            shell_process.env("CARGO_TARGET_DIR", path);
+        }
+    }
+
+    shell_process
+        .current_dir(&tmp_dir)
+        .status()
+        .context("Cannot start shell")?;
+
+    #[cfg(windows)]
+    if config.editor.is_some() {
+        unsafe {
+            cargo_temp_bindings::Windows::Win32::SystemServices::FreeConsole();
+        }
+    }
+
+    Ok(())
+}
+
+pub fn clean_up(
+    delete_file: PathBuf,
+    tmp_dir: TempDir,
+    worktree_branch: Option<Option<String>>,
+) -> Result<()> {
+    if !delete_file.exists() {
+        println!(
+            "Project directory preserved at: {}",
+            tmp_dir.into_path().display()
+        );
+    } else if worktree_branch.is_some() {
+        let mut command = process::Command::new("git");
+        command
+            .args(["worktree", "remove"])
+            .arg(&tmp_dir.path())
+            .arg("--force");
+        ensure!(
+            command.status().context("Could not start git")?.success(),
+            "Cannot remove working tree"
+        );
+    }
+
+    Ok(())
+}
+
+pub fn get_shell() -> String {
+    #[cfg(unix)]
+    {
+        env::var("SHELL").unwrap_or_else(|_| "/bin/sh".to_string())
+    }
+
+    #[cfg(windows)]
+    {
+        env::var("COMSPEC").unwrap_or_else(|_| "cmd".to_string())
+    }
+}
