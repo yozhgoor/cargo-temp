e31d40c973b1e001ae628803285a554e000ba168
Yozhgoor <yozhgoor@outlook.com>
2021-08-20 21:29:10 +0200

    Better use of the Option<u32>

--- src/run.rs
+++ src/run.rs
@@ -49,19 +49,12 @@ pub fn generate_tmp_project(
             "Cannot create working tree"
         );
     } else if let Some(url) = git {
-        let depth = if let Some(depth) = git_repo_depth {
-            depth
-        } else {
-            1
-        };
-
         let mut command = process::Command::new("git");
-        command
-            .arg("clone")
-            .arg(url)
-            .arg(&tmp_dir.as_ref())
-            .arg("--depth")
-            .arg(depth.to_string());
+        command.arg("clone").arg(url).arg(&tmp_dir.as_ref());
+
+        if let Some(depth) = git_repo_depth {
+            command.arg("--depth").arg(depth.to_string());
+        }
 
         ensure!(
             command.status().context("Could not start git")?.success(),
