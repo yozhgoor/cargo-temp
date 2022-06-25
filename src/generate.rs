ec8cf48d03968e2ed84ea47621afecc3d51173d5
yozhgoor <yozhgoor@outlook.com>
2022-06-25 18:44:33 +0200

    Update `cargo-generate` to the v0.15

--- src/generate.rs
+++ src/generate.rs
@@ -2,76 +2,48 @@
 
 use anyhow::{ensure, Result};
 use clap::Parser;
-use std::path::{Path, PathBuf};
+use cargo_generate::{GenerateArgs, TemplatePath};
+use std::path::PathBuf;
 
 use crate::{clean_up, generate_delete_file, start_shell, start_subprocesses, Config};
 
 #[derive(Clone, Debug, Parser)]
 pub struct Args {
+    #[clap(flatten)]
+    pub template_path: TemplatePath,
+
     /// List defined favorite templates from the config
     #[clap(
         long,
-        conflicts_with = "git",
-        conflicts_with = "subfolder",
-        conflicts_with = "path",
-        conflicts_with = "branch",
-        conflicts_with = "name",
-        conflicts_with = "force",
-        conflicts_with = "template-values-file",
-        conflicts_with = "silent",
-        conflicts_with = "vcs",
-        conflicts_with = "lib",
-        conflicts_with = "bin",
-        conflicts_with = "ssh-identity",
-        conflicts_with = "define",
-        conflicts_with = "init"
+        action,
+        conflicts_with_all(&[
+            "git", "path", "subfolder", "branch",
+            "name",
+            "force",
+            "silent",
+            "vcs",
+            "lib",
+            "bin",
+            "define",
+            "init",
+            "templates-values-file",
+            "ssh-identity",
+        ])
     )]
     pub list_favorites: bool,
-
-    /// Generate a favorite template as defined in the config. In case the favorite is undefined,
-    /// use in place of the `--git` option, otherwise specifies the subfolder
-    #[clap(name = "favorite")]
-    pub favorite: Option<String>,
-
-    /// Specifies a subfolder within the template repository to be used as the actual template.
-    #[clap(name = "subfolder")]
-    pub subfolder: Option<String>,
-
-    /// Git repository to clone template from. Can be a URL (like
-    /// `https://github.com/rust-cli/cli-template`), a path (relative or absolute), or an
-    /// `owner/repo` abbreviated GitHub URL (like `rust-cli/cli-template`).
-    ///
-    /// Note that cargo generate will first attempt to interpret the `owner/repo` form as a
-    /// relative path and only try a GitHub URL if the local path doesn't exist.
-    #[clap(name = "git", short, long, conflicts_with = "subfolder")]
-    pub git: Option<String>,
-
-    /// Local path to copy the template from. Can not be specified together with --git.
-    #[clap(
-        short,
-        long,
-        conflicts_with = "git",
-        conflicts_with = "favorite",
-        conflicts_with = "subfolder"
-    )]
-    pub path: Option<PathBuf>,
-
-    /// Branch to use when installing from git
-    #[clap(short, long)]
-    pub branch: Option<String>,
-
+    
     /// Directory to create / project name; if the name isn't in kebab-case, it will be converted
     /// to kebab-case unless `--force` is given.
-    #[clap(long, short)]
+    #[clap(long, short, value_parser)]
     pub name: Option<String>,
 
     /// Don't convert the project name to kebab-case before creating the directory.
     /// Note that cargo generate won't overwrite an existing directory, even if `--force` is given.
-    #[clap(long, short)]
+    #[clap(long, short, action)]
     pub force: bool,
 
     /// Enables more verbose output.
-    #[clap(long, short)]
+    #[clap(long, short, action)]
     pub verbose: bool,
 
     /// Pass template values through a file
@@ -120,9 +92,7 @@ impl Args {
             .prefix("tmp-")
             .tempdir_in(&config.temporary_project_dir)?;
 
-        let args = self.convert(tmp_dir.path());
-
-        cargo_generate::generate(args)?;
+        self.cargo_generate()?;
 
         let delete_file = generate_delete_file(tmp_dir.path())?;
 
@@ -137,14 +107,10 @@ impl Args {
         Ok(())
     }
 
-    fn convert(self, destination_path: &Path) -> cargo_generate::Args {
-        cargo_generate::Args {
+    fn cargo_generate(self) -> Result<()> {
+        cargo_generate::generate(GenerateArgs {
+            template_path: self.template_path,
             list_favorites: self.list_favorites,
-            favorite: self.favorite,
-            subfolder: self.subfolder,
-            git: self.git,
-            path: self.path,
-            branch: self.branch,
             name: self.name,
             force: self.force,
             verbose: self.verbose,
@@ -156,13 +122,13 @@ impl Args {
             bin: self.bin,
             ssh_identity: self.ssh_identity,
             define: self.define,
-            // Ignore `--init` since we create a temporary directory.
-            init: false,
-            // Force the destination path for the template to be the temporary directory.
-            destination: Some(destination_path.to_path_buf()),
+            // Use `init` to init the template as the created temporary directory.
+            init: true,
+            // Ignore `--destination` since we create a temporary directory.
+            destination: None,
             force_git_init: self.force_git_init,
             // todo: add `allow_commands`.
             allow_commands: false,
-        }
+        })
     }
 }
