fc79d1194a360352afd36b558778aa10d43e24a7
yozhgoor <yozhgoor@outlook.com>
2025-11-15 22:19:45 +0100

    Replace `concat!` by verbose mode for the regex

--- src/dependency.rs
+++ src/dependency.rs
@@ -29,18 +29,18 @@ impl FromStr for Dependency {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         static RE: LazyLock<Regex> = LazyLock::new(|| {
-            let pattern = concat!(
-                r"^",
-                r"(?:(?P<url>(?:https?|ssh)://(?:[^:@]+(?::[^@]+)?@)?[^/:@?#+=]+(?:/[^#+=]+)+(?:\.git)?)",
-                r"|",
-                "(?P<name>[^+=]+))",
-                r"(?:#(?P<git_ref>[^=+]+))?",
-                r"(?:=(?P<version>(?:>=|<=|>|<|=|~)?[0-9A-Za-z.\-]+))?",
-                r"(?P<default_features>\+[^+]?)?",
-                r"(?P<features>(?:\+[^+]+)*)$",
-            );
-
-            Regex::new(pattern).expect("dependency regex must compile")
+            Regex::new(
+                r"(?x)^
+(?:(?P<url>(?:https?|ssh)://(?:[^:@]+(?::[^@]+)?@)?[^/:@?\#+=]+(?:/[^\#+=]+)+(?:\.git)?)
+|
+(?P<name>[^+=]+))
+(?:\#(?P<git_ref>[^=+]+))?
+(?:=(?P<version>(?:>=|<=|>|<|=|~)?[0-9A-Za-z.\-]+))?
+(?P<default_features>\+[^+]?)?
+(?P<features>(?:\+[^+]+)*)$
+",
+            )
+            .expect("dependency regex must compile")
         });
 
         match RE.captures(s) {
